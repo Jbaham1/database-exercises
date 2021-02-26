6e5693660f971611152421062bc5ed3866ae3bd4
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-02-26 16:39:35 -0600

    group by exercise

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -6,3 +6,13 @@ SELECT DISTINCT last_name, first_name
 FROM employees
 WHERE last_name LIKE 'E%e'
 group by last_name, first_name;
+
+SELECT last_name, COUNT(last_name)
+FROM employees
+where last_name like '%q%' AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
+
+SELECT CONCAT(COUNT(gender), ' ', gender)
+FROM employees
+WHERE first_name IN ('Irena','Vidya','Maya')
+GROUP BY gender;
