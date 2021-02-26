d7d9c9eb70bfff6cde7b6d708dfc1e64b1cf8bd4
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-02-26 10:50:01 -0600

    90's hire date correction

--- where_exercises.sql
+++ where_exercises.sql
@@ -6,7 +6,7 @@ WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 
 SELECT *
 FROM employees
-WHERE hire_date BETWEEN 1990 AND 2000;
+WHERE hire_date BETWEEN 1989-12-31 AND 2000-01-1;
 
 SELECT *
 FROM employees
