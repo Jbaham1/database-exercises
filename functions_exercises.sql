a20938d9a92d24c6b10d0c9601ec6597f1a5ac2a
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-02-26 14:33:27 -0600

    updates

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,10 +1,11 @@
 USE employees;
 
-SELECT emp_no, CONCAT(first_name, ' ', last_name) AS name
+SELECT *, birth_date, CONCAT_WS(' ', first_name, last_name) AS name
 FROM employees
-WHERE last_name LIKE 'E%e';
+WHERE last_name LIKE 'E%e'
+ORDER BY emp_no DESC;
 
-SELECT emp_no, CONCAT(first_name, ' ', last_name) AS name, DATEDIFF(CURDATE(), hire_date)
+SELECT *, DATEDIFF(CURDATE(), hire_date)
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
   AND birth_date LIKE '%-12-25'
