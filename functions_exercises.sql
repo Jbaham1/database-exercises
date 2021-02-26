41826b4b35cda1d80a8e8aca90924f54224acfb2
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-02-26 13:58:04 -0600

    function finish

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,17 +1,11 @@
 USE employees;
-SELECT * FROM EMPLOYEES ORDER BY last_name, first_name;
-SELECT * FROM employees WHERE first_name IN ('Irena','Vidya','Maya' ) ORDER BY last_name, first_name;
 
-SELECT * FROM employees WHERE last_name like 'E%' OR last_name like '%E' ;
+SELECT emp_no, CONCAT(first_name, ' ', last_name) AS name
+FROM employees
+WHERE last_name LIKE 'E%e';
 
-SELECT * FROM employees WHERE last_name like 'E%' AND last_name like '%E' ORDER BY emp_no DESC;
-
-
-SELECT * from employees where hire_date like '199%';
-
-SELECT * from employees where birth_date like '%12-25';
-
-SELECT * from employees where birth_date like '%12-25' AND hire_date like '199%' ORDER BY birth_date, hire_date DESC;
-
-
-SELECT * from employees where last_name like '%q%' AND last_name NOT LIKE '%qu%';
+SELECT emp_no, CONCAT(first_name, ' ', last_name) AS name, DATEDIFF(CURDATE(), hire_date)
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+  AND birth_date LIKE '%-12-25'
+ORDER BY birth_date, hire_date DESC;
