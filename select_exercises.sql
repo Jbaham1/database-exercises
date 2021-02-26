de642f635905043d6dc16e0f9d5cbee3d2fb6a68
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-02-26 09:02:55 -0600

    nvmd

--- select_exercises.sql
+++ select_exercises.sql
@@ -5,5 +5,6 @@ SELECT name FROM albums WHERE artist = 'Pink Floyd';
 SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 # The genre for Nevermind
 # Which albums were released in the 1990s
+SELECT genre FROM albums WHERE name = 'Nevermind';
 # Which albums had less than 20 million certified sales
 # All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"?
