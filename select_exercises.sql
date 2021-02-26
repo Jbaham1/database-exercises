fc2e87d1e4dfa12114ab40dc513d889d63c702c5
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-02-26 09:02:29 -0600

    select Sgt pepper

--- select_exercises.sql
+++ select_exercises.sql
@@ -2,6 +2,7 @@ USE codeup_test_db;
 # The name of all albums by Pink Floyd.
 SELECT name FROM albums WHERE artist = 'Pink Floyd';
 # The year Sgt. Pepper's Lonely Hearts Club Band was released
+SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 # The genre for Nevermind
 # Which albums were released in the 1990s
 # Which albums had less than 20 million certified sales
