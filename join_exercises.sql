f5084751793c9812352ff67425ce7a213fd72a5b
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-03-01 15:48:19 -0600

    head count

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,15 +1,27 @@
 USE employees;
 
-SELECT dept_name AS 'Department Name', CONCAT(first_name, ' ', last_name)AS 'Department Manager'
-FROM departments AS d
-JOIN dept_manager dm on d.dept_no = dm.dept_no
-JOIN employees e on dm.emp_no = e.emp_no
-WHERE dm.to_date = '9999-01-01'
-ORDER BY d.dept_name;
-
-SELECT dept_name AS 'Department Name', CONCAT(first_name, ' ', last_name)AS 'Department Manager'
+SELECT dept_name AS 'Department Name', CONCAT(first_name, ' ', last_name) AS 'Department Manager'
 FROM departments AS d
          JOIN dept_manager dm on d.dept_no = dm.dept_no
          JOIN employees e on dm.emp_no = e.emp_no
-WHERE dm.to_date = '9999-01-01' AND e.gender = 'F'
+WHERE dm.to_date = '9999-01-01'
 ORDER BY d.dept_name;
+
+SELECT dept_name AS 'Department Name', CONCAT(first_name, ' ', last_name) AS 'Department Manager'
+FROM departments AS d
+         JOIN dept_manager dm on d.dept_no = dm.dept_no
+         JOIN employees e on dm.emp_no = e.emp_no
+WHERE dm.to_date = '9999-01-01'
+  AND e.gender = 'F'
+ORDER BY d.dept_name;
+
+SELECT t.title AS 'Title', COUNT(*) AS 'Head Count'
+FROM employees e
+         JOIN dept_emp de on e.emp_no = de.emp_no
+         JOIN departments d on de.dept_no = d.dept_no
+         JOIN titles t on e.emp_no = t.emp_no
+WHERE de.to_date = '9999-01-01'
+  AND t.to_date = '9999-01-01'
+  AND d.dept_name = 'Customer Service'
+GROUP BY t.title
+ORDER BY t.title;
