ff735ad74ee9069bd2125f2359b47190150ec95c
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-03-01 15:40:31 -0600

    gender

--- join_exercises.sql
+++ join_exercises.sql
@@ -6,3 +6,10 @@ JOIN dept_manager dm on d.dept_no = dm.dept_no
 JOIN employees e on dm.emp_no = e.emp_no
 WHERE dm.to_date = '9999-01-01'
 ORDER BY d.dept_name;
+
+SELECT dept_name AS 'Department Name', CONCAT(first_name, ' ', last_name)AS 'Department Manager'
+FROM departments AS d
+         JOIN dept_manager dm on d.dept_no = dm.dept_no
+         JOIN employees e on dm.emp_no = e.emp_no
+WHERE dm.to_date = '9999-01-01' AND e.gender = 'F'
+ORDER BY d.dept_name;
