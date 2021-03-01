29dabc4db9f72ae410050076fc6f9e8a61dd0558
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-03-01 11:54:43 -0600

    join exercise

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,12 +1,11 @@
-CREATE DATABASE  IF NOT EXISTS join_test_db;
-USE join_test_db;
-CREATE TABLE roles (
+Use employees;
+CREATE TABLE IF NOT EXISTS roles (
                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
                        name VARCHAR(100) NOT NULL,
                        PRIMARY KEY (id)
 );
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
                        name VARCHAR(100) NOT NULL,
                        email VARCHAR(100) NOT NULL,
@@ -20,15 +19,10 @@ INSERT INTO roles (name) VALUES ('author');
 INSERT INTO roles (name) VALUES ('reviewer');
 INSERT INTO roles (name) VALUES ('commenter');
 
-INSERT INTO users (name, email, role_id)
-VALUES
+INSERT INTO users (name, email, role_id) VALUES
 ('bob', 'bob@example.com', 1),
 ('joe', 'joe@example.com', 2),
 ('sally', 'sally@example.com', 3),
 ('adam', 'adam@example.com', 3),
 ('jane', 'jane@example.com', null),
 ('mike', 'mike@example.com', null);
-
-SELECT users.name as user_name, roles.name as role_name
-FROM users
-JOIN roles ON users.role_id = roles.id;
